8eb642343398dd857a4f23817e9ede3b6e54fd4f
Diegolopes0712 <diego.lopes12@yahoo.com>
2025-04-30 13:59:55 +0200

    Update Chercheanimal.c
    - Changement de nom d'une variable (le a du scanf en b).
    - Correction de certaines erreurs du programme.
    - Création de la boucle récursive en cas d'erreur.

--- Chercheanimal.c
+++ Chercheanimal.c
@@ -1,35 +1,37 @@
 int chercheAnimal(Animal a){
     printf("que voulez vous chercher ?");
-    scanf("%s" ,a);
-    if(a=="age"){
+    scanf("%s" ,b);
+    if(b=="age"){
     if(a.anneenaissance >= 2023){ //Les animaux nés en 2023 ont 2 ans en 2025
         printf("animal jeune");
     }
-    else if (a.annéenaissance <= 2015){ // Les animaux nés en 2015 ont 10 ans en 2025
+    else if (a.anneenaissance <= 2015){ // Les animaux nés en 2015 ont 10 ans en 2025
         printf("animal sénior");
     }
     else {
         printf("animal adulte");
     }
-  else if(a=="espece"){
-  if(espece == 1){ // 1 correspond à l'espèce présente
+    }
+  else if(b=="espece"){
+  if(a.espece == 1){ // 1 correspond à l'espèce présente
       printf("%s", a.espece);
   }
       else{
           printf("erreur");
+          chercheAnimal(a);
       }
   }
-  else if(a=="nom"){
+  else if(b=="nom"){
       if(a.nom==1){ // ici 1 correspond au nom existant
           printf("%s", a.nom);
       }
       else{
           printf("erreur");
+          chercheAnimal(a);
       }
     }
  else{
-        printf("erreur");
-    printf("que voulez vous chercher ?");
-    scanf("%s" ,a);
+      printf("erreur");
+      chercheAnimal(a);
     }
 }
