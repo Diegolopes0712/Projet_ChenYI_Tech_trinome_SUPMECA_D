40dd4fe28549163801f87e4cdb260d98cf1e1e6a
Diegolopes0712 <diego.lopes12@yahoo.com>
2025-05-07 14:13:14 +0200

    Update ordredecroissant.c
    Amélioration et Finalisation de la seconde variante du programme.

--- ordredecroissant.c
+++ ordredecroissant.c
@@ -1,45 +1,51 @@
 #include "chenil.h"
 
-int inv_nb_desc(int*tab){
-
-int nbanimal=0;
-int nbchat = 0;
-int nbchien = 0; 
-int nbautruche = 0;
-int nbhamster =0;
-int *c
-for(int i=0; i<50; i++){
-	if(espece=="chien"){
+int inv_nb_desc(Animal* tab){
+    int nbchat = 0;
+    int nbchien = 0; 
+    int nbautruche = 0;
+    int nbhamster = 0;
+    int c[4];
+    int max=0;
+    for(int i=0; i<50; i++){
+       if((strcmp(tab[i].espece,"chien"))==0){
 		nbchien++;
-		}
-	if(espece=="chat"){
+       }
+       else if((strcmp(tab[i].espece,"chat"))==0){
 		nbchat++;
-		}
-	if(espece=="autruche"){
+       }
+       else if((strcmp(tab[i].espece,"autruche"))==0){
 		nbautruche++;
-		}
-	if(espece=="hamster"){
-		nbhamster++;
-		}
-	nbanimal++;
-	}
-for(int j=0; j<4; j++){
-if(tab[nbchien]>tab[nbchat]){
-	c[i]=tab[nbchat];
-	nbchat = nbchat+1
-else{
- c[i]=tab[nbchien];
- nbchien=nbchien+1;
-}
-}
-if(tab[nbautruche]>tab[nbhamster]){
-	c[i]=tab[nbhamster];
-	nbhamster= nbhamster+1
-else{
- c[i]=tab[nbautruche];
- nbautruche=nbautruche+1;
-}
-  printf("%d", tab[animaux]);
-}
-}
-printf(" nombre d'animaux : %d \n", nbanimaux);
+       }
+       else {
+            nbhamster++;
+       }
+    }
+    c[0]=nbchien;
+    c[1]=nbchat;
+    c[2]=nbautruche;
+    c[3]=nbhamster;
+    for(int m=0; m<=2; m++){
+       for(int j=0; j<=2; j++){
+          max=j;
+          for(int k=j+1; k<=3; k++){
+             if(c[k]>c[max]){
+               max=k;
+	     }
+          }
+          if(max!=j){
+            int tmp = c[j];
+            c[j] = c[max];
+            c[max] = tmp;
+          }
+       }
+    }
+
+    printf(" le classement du nombre d'animaux du refuge par espèce est : 1) %d 2) %d 3) %d 4) %d ! ", c[0], c[1], c[2], c[3]);
+
+
+
+    
+
+
+       
