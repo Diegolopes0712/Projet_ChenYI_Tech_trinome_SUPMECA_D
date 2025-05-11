bd0e3424911c1b11b6e11419bb222241276c0b8f
Lampeahuile230 <noebrnak95@gmail.com>
2025-05-11 20:11:06 +0200

    Update bout de code fonctionnel.c

--- bout de code fonctionnel.c
+++ bout de code fonctionnel.c
@@ -106,31 +106,43 @@ typedef struct{ //Caractéristique d'un animal
     
     Animal constructeurAnimal(){ //ici on crée un animal
 	Animal a;
-	char* i;
+	char i, oui, non;
 	int nbE;
 	a.espece=nbE;
-	printf(" Numéro d'identification ? ");
-	scanf("%d", &a.numid);
-	printf(" Quel est son nom ? ");
-	scanf("%s", a.nom);
-	printf(" En quelle année est-t-il né ? ");
-	scanf("%d", &a.anneenaissance);
-	printf(" Quelle est son Espèce ? ");
-	scanf("%d", &nbE);
+
+	printf(" Numéro d'identification ? (6 chiffres) \n ");
+	  scanf("%d", &a.numid);
+	printf(" Quel est son nom ? \n");
+	  scanf("%s", a.nom);
+	printf(" En quelle année est-t-il né ? \n ");
+	  scanf("%d", &a.anneenaissance);
+	    while(a.anneenaissance<=2005 || a.anneenaissance>=2025);{
+	       printf(" Veuillez saisir une année de naissance cohérente ! \n");
+	         scanf("%d", &a.anneenaissance);
+	    }
+	    
+	printf(" Quelle est son Espèce ? 0/chien 1/chat 2/hamster 3/autruche \n");
+	  scanf("%d", &nbE);
+	  
 	printf(" Quel est son poids ? ");
-	scanf("%f", &a.poids);
-	printf(" Un commentaire ? oui ou non ");
-	scanf("%s", i);
-	if(i=="oui"){
+	  scanf("%f", &a.poids);
+	    while(a.poids<=0.0 || a.poids>=200.0);{
+	       printf("Veuillez saisir un poids cohérent ! \n");
+	         scanf("%f", &a.poids);
+	    }
+	    
+	printf(" Un commentaire ? oui/0 ou non/1 ");
+	  scanf("%s", &i);
+	if(i==oui){
 	   printf(" Quel commentaire ? ");
-	   scanf("%s", a.commentaire);
+	     scanf("%s", a.commentaire);
 	}
-	else if(i!="oui" && i!="non"){
+	else if(i!=oui && i!=non){
 	        printf(" Veuillez répondre par oui ou par non s'il vous plait ! ");
-		scanf("%s", i);
-		if(i=="oui"){
+		      scanf("%s", &i);
+		if(i==oui){
 	           printf(" Quel commentaire ? ");
-	           scanf("%s", a.commentaire);
+	             scanf("%s", a.commentaire);
 	        }
 		else{
 			return a;
@@ -186,13 +198,78 @@ int liste(int listA){
 
 
 
+
+void modifier_animal(Animal a){
+     int choix,numid,anneenaissance,espece;
+     float poids;
+     printf(" Que voulez-vous modifier sur l'animal ? \n Tapez 1 pour modifier le numéro d'identification \n Tapez 2 pour modifier le nom \n Tapez 3 pour modifier l'année de naissance \n Tapez 4 pour modifier l'espèce \n Tapez 5 pour modifier le poids \n Tapez 6 pour modifier le commentaire \n ");
+     scanf("%d", &choix);
+     if(choix==1){
+       printf(" Quel est son nouveau numéro d'identification ? \n ");
+       scanf("%d", &numid);
+       if(numid>=1){
+         a.numid=numid;
+       }
+       else{
+           printf(" Veuillez saisir un nombre entier supérieur ou égal à 1 ! \n ");
+           modifier_animal(a);
+       } 
+     }
+     else if(choix==2){
+       printf(" Quel est son nouveau nom ? \n ");
+       scanf("%s", a.nom);
+     }
+     else if(choix==3){
+       printf(" Quel est sa nouvelle année de naissance ? \n ");
+       scanf("%d", &anneenaissance);
+       if(anneenaissance>=2000){
+         a.anneenaissance=anneenaissance;
+       }
+       else{
+           printf(" Veuillez saisir un nombre entier supérieur ou égal à 2000 ! \n ");
+           modifier_animal(a);
+       } 
+     }
+     else if(choix==4){
+       printf(" Quel est sa nouvelle espèce ? \n ");
+       scanf("%d", &espece);
+       if(espece>=0 && espece<=3){
+         a.espece=espece;
+       }
+       else{
+           printf(" Veuillez saisir un nombre entier supérieur ou égal à 0 et inférieur ou égal à 3 ! \n ");
+           modifier_animal(a);
+       } 
+     }
+     else if(choix==5){
+       printf(" Quel est son nouveau poids ? \n ");
+       scanf("%f", &poids);
+       if(poids>0.0){
+         a.poids=poids;
+       }
+       else{
+           printf(" Veuillez saisir un nombre décimal strictement supérieur à 0.0 ! \n ");
+           modifier_animal(a);
+       } 
+     }
+     else if(choix==6){
+       printf(" Quel est son nouveau commentaire ? \n ");
+       scanf("%s", a.commentaire); 
+     }
+     else{
+         printf(" Veuillez saisir un nombre entier situé entre 1 et 6 inclus ! \n ");
+         modifier_animal(a);
+     }
+}   
+
+
 int main(){
     int nb, rep, nbPu, nbPl, code, A;
     int nbAnimal, choix, C, numid, anneenaissance;
     float poids;
     Espece espece;
     char commentaire, nom;
-    Animal  Action1, Action2;
+    Animal  Action1, Action2, a;
     int taille=50;
     float df;
     Animal tab[taille];
@@ -307,6 +384,10 @@ printf("                          |___/                         \n\n ");
         
                        case(3):
                           printf("Votre action: %d \n" , nbPl);
+                             constructeurAnimal(a);
+                                printf(" %d \n %s \n %d \n %d \n %f \n %s \n ",a.numid,a.nom,a.anneenaissance,a.espece,a.poids,a.commentaire);
+                             modifier_animal(a);
+                                printf(" %d \n %s \n %d \n %d \n %f \n %s \n ",a.numid,a.nom,a.anneenaissance,a.espece,a.poids,a.commentaire);
                           return 0;
                           break;
                 
