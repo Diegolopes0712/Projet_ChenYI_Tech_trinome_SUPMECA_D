6852c2d61fa7bd487dad4278f78bf57e6e184d1d
Lampeahuile230 <noebrnak95@gmail.com>
2025-05-11 14:50:46 +0200

    Update Adopt:Depot.c

--- Adopt:Depot.c
+++ Adopt:Depot.c
@@ -3,7 +3,7 @@
  //fonction pour adopter un animal//
  Animal adoptAnimal(int chxA){
     int nbAnimal;
-    printf("Choisissez 0 ou 1 ou 2 ou 3 respectivement pour chien, chat, hamster, autruche \n et indiquez 4, "stop" pour arrêter la demande \n ");
+    printf("Choisissez 0 ou 1 ou 2 ou 3 respectivement pour chien, chat, hamster, autruche \n et indiquez 4(stop), pour arrêter la demande \n ");
     scanf("%d", &chxA);
        switch(chxA){
         case 0:
@@ -27,7 +27,7 @@
            printf("Vous venez d'arrêter la demande d'adoption. \n");
            break;
         default:
-           printf("La valeur entrée ne correspond à aucun des choix possibles" \n);
+           printf("La valeur entrée ne correspond à aucun des choix possibles \n");
            exit(2);
            break;
        } 
@@ -39,40 +39,40 @@
 
  //fonction pour déposer un animal//
  Animal depotAnimal(int numid, char* nom, Espece espece, int anneenaissance, float poids, char* commentaire){
-   int nbAdepot, nbAnimal, sûr?;
+   int nbAdepot, nbAnimal, sûr;
    printf("Combien d'animaux voulez-vous déposer ? \n");
    scanf("%d",&nbAdepot);
        
        if(nbAdepot=1 && nbAnimal+nbAdepot<=50){  //Pour déposer 1 animal//
        printf("Votre animal a bien été déposé. \n");
        nbAnimal+=nbAdepot;
-        return nbAnimal;
+        printf("Il y a %d animaux dans le refuge. \n", nbAnimal);
        }
-       else(nbAdepot>1){
+       else if(nbAdepot>1){
            if(nbAnimal+nbAdepot<=50){
              printf("Vos animaux ont bien été déposés ! \n");
              nbAnimal+=nbAdepot;
-             return nbAnimal;
+             printf("Il y a %d animaux dans le refuge. \n", nbAnimal);
            }
            else{
              nbAdepot=(nbAnimal+nbAdepot)-50;
-             printf("Vous pouvez déposer %d animaux à cause du manque de place ! \n, nbAdepot");
+             printf("Vous pouvez déposer %d animaux à cause du manque de place ! \n", nbAdepot);
              printf("Voulez-vous quand même les déposer ? \n oui:1 ou non:2 \n");
-             scanf("%d",&sûr?);
-               if(sûr?==1){
+             scanf("%d",&sûr);
+               if(sûr==1){
                   printf("Vos animaux ont bien été déposés ! \n");
                   nbAnimal=50;
-                  return nbAnimal;
+                  printf("Il y a %d animaux dans le refuge. \n", nbAnimal);
                }
-               else if(sûr?==2){
+               else if(sûr==2){
                   printf("Vos animaux rentrent avec vous aujourd'hui ! \n");
-                  return nbAnimal;
+                  printf("Il y a %d animaux dans le refuge. \n", nbAnimal);
                } 
                else{
                   printf("Veuillez saisir une valeur cohérente: 1 ou 2 \n");
                   exit(1); 
-           }
-       }
+               }
+            }
        
   
    
@@ -96,7 +96,7 @@
  
  int main(){
     int nbAnimal, choix, Action1, Action2;
-    printf("Vous voulez un chat, un chien, un hamster, ou même une autruche ?!?! \n ChenYl Tech est là pour vous !! \n);
+    printf("Vous voulez un chat, un chien, un hamster, ou même une autruche ?!?! \n ChenYl Tech est là pour vous !! \n");
     printf("Quelle action voulez vous effectuer ? \n 1:Adopter ou 2:déposer ? \n");
     scanf("%d",&choix);
        if(choix==1){
@@ -109,5 +109,6 @@
        }
  return 0;
  }
+ 
  
  
